35d19227a33be600574da616f0fc34a9070a3a0d
jydeveloper <lukaryu@naver.com>
2022-05-08 19:52:53 +0900

    navSlice 오타수정, 출발지/목적지 정해지면 지도 줌수치 수정

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import MapView, { Marker } from 'react-native-maps';
@@ -10,9 +10,20 @@ import { GOOGLE_MAPS_APIKEY } from '@env';
 const Map = () => {
 	const origin = useSelector(selectOrigin);
 	const destination = useSelector(selectDestination);
+	const mapRef = useRef(null);
+
+	useEffect(() => {
+		if (!origin || !destination) return;
+
+		// Zoom & fit to markers
+		mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+			edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+		});
+	}, [origin, destination]);
 
 	return (
 		<MapView
+			ref={mapRef}
 			style={tw`flex-1`}
 			mapType="mutedStandard"
 			initialRegion={{
@@ -28,7 +39,7 @@ const Map = () => {
 					destination={destination.description}
 					apikey={GOOGLE_MAPS_APIKEY}
 					strokeWidth={3}
-					strokeColor="hotpink"
+					strokeColor={'#000000'}
 				/>
 			)}
 			{origin?.location && (
@@ -37,9 +48,20 @@ const Map = () => {
 						latitude: origin.location.lat,
 						longitude: origin.location.lng,
 					}}
-					title="Origin"
+					title="출발지"
 					description={origin.description}
 					identifier="origin"
+				/>
+			)}
+			{destination?.location && (
+				<Marker
+					coordinate={{
+						latitude: destination.location.lat,
+						longitude: destination.location.lng,
+					}}
+					title="도착지"
+					description={destination.description}
+					identifier="destination"
 				/>
 			)}
 		</MapView>
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -26,7 +26,7 @@ export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.
 
 // Selectors
 export const selectOrigin = (state) => state.nav.origin;
-export const selectDestination = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
 export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
 
 export default navSlice.reducer;
